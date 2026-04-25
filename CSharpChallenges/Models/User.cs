ad99e9906346779b5031017dc172816258ba1b3c
knarf12312 <gonzalesjosefrank22@gmail.com>
2026-04-25 23:47:35 +0800

    db connection

---
+++ CSharpChallenges/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpChallenges.Models
+{
+    public class User
+    {
+        int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Pin { get; set; }
+        public decimal Balance { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
--- CSharpChallenges/Program.cs
+++ CSharpChallenges/Program.cs
@@ -1,6 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CSharpChallenges.UI;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+//"MyDbConnection": "Server=localhost\\SQLEXPRESS;Database=inventory_system;User Id=franky;Password=password123;TrustServerCertificate=True;" //laptop
 
 
 var display = new Menu();
-display.Display();
+//display.Display();
+
+
+
+var builder = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+var config = builder.Build();
+
+string conn = config.GetConnectionString("DefaultConnection");
+
+Console.WriteLine(conn);
+
+using (SqlConnection connection = new SqlConnection(conn))
+{
+    connection.Open();
+
+    string query = "SELECT * FROM TblUser";
+
+    using (SqlCommand command = new SqlCommand(query, connection))
+    using (SqlDataReader reader = command.ExecuteReader())
+    {
+        while (reader.Read())
+        {
+            Console.WriteLine($"ID: {reader["UserId"]}, Name: {reader["FirstName"]}");
+        }
+    }
+}
