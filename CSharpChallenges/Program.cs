df6e24c2f6cbe80d3ccfb949bac0c421afee3b7c
knarf22 <gonzalesjosefrank22@gmail.com>
2026-06-18 23:36:39 +0800

    123

--- CSharpChallenges/Program.cs
+++ CSharpChallenges/Program.cs
@@ -16,7 +16,7 @@ var config = builder.Build();
 
 string conn = config.GetConnectionString("DefaultConnection");
 
-// 🔥 CREATE DB OPTIONS (THIS IS WHAT YOU ARE MISSING)
+// 🔥 CREATE DB OPTIONS (THIS IS WHAT YOU ARE MISSING) yeah
 var options = new DbContextOptionsBuilder<AppDbContext>()
     .UseSqlServer(conn)
     .Options;
